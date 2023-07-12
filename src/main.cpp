1b3348db8412e263d33b1898f4e040048b5d9e7f
unknown <gilantonio656@gmail.com>
2023-07-11 19:31:18 -0600

    implementing interrupts

--- src/main.cpp
+++ src/main.cpp
@@ -18,9 +18,35 @@ const int DHTPin = 4;
 DHT dht(DHTPin, DHTTYPE);
 void ver_pantalla(String, String);
 
+hw_timer_t *My_timer = NULL;
+void IRAM_ATTR onTimer(){
+  //
+  float h = dht.readHumidity();//read humidity
+ 	float t = dht.readTemperature();// Read temperature as Celsius (the default)
+
+	String str_h = String(h);
+	String str_tem = String(t);
+	
+  String serial_str_h = "Humidity: " + str_h;
+  String serial_str_tem = "temperature: " + str_tem;
+  
+  //Monitoreo Serial
+	Serial.println(serial_str_h);
+	Serial.println(serial_str_tem);
+
+  //Monitoreo pantalla LCD
+  ver_pantalla(str_h,str_tem);
+}
+
 void setup() {
   Serial.begin(115200);
   pinMode(DHTPin,INPUT);
+
+  //timer interrupt
+  My_timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(My_timer, &onTimer, true);
+  timerAlarmWrite(My_timer, 10000000, true);//each 10s
+  timerAlarmEnable(My_timer); //Just Enable
   
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Address 0x3C for 128x32
   // Show image buffer on the display hardware.
@@ -38,36 +64,24 @@ void setup() {
 
 void loop() {
 
-  float h = dht.readHumidity();//read humidity
- 	float t = dht.readTemperature();// Read temperature as Celsius (the default)
-
-	String str_h = String(h);
-	String str_tem = String(t);
-	
-  String serial_str_h = "Humidity: " + str_h;
-  String serial_str_tem = "temperature: " + str_tem;
-  
-  //Monitoreo Serial
-	Serial.println(serial_str_h);
-	Serial.println(serial_str_tem);
-
-  //Monitoreo pantalla LCD
-  ver_pantalla(str_h,str_tem);
-  
-
 }
 
 void ver_pantalla(String str_h, String str_tem) {
 
+  display.clearDisplay();
   //Monitoreo por pantalla
-	display.setTextSize(2);
+	display.setTextSize(3);
   display.setTextColor(SSD1306_WHITE);
-  display.setCursor(25,15);
+  display.setCursor(20,20);
   display.print(str_h + " %");
   display.display();
-  display.setCursor(25,35);
+  delay(3000);//clear each 3s
+	display.clearDisplay();
+
+  display.setTextSize(3);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(20,20);
   display.print(str_tem + " C");
   display.display();
-  delay(800);//clear each 800ms
-	display.clearDisplay();
+  
 }
